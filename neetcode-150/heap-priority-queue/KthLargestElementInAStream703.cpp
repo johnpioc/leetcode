eb39576d305da9259ddeb036adba03d866d05601
John Pioc <johnpioc15@gmail.com>
2025-11-26 17:12:34 +1000

    #703: Kth largest element in a stream

---
+++ neetcode-150/heap-priority-queue/KthLargestElementInAStream703.cpp
@@ -0,0 +1,38 @@
+class KthLargest {
+public:
+    priority_queue<int, vector<int>, greater<int>> pq;
+    int k_;
+
+    KthLargest(int k, vector<int>& nums) {
+        this->k_ = k;
+
+        for (int num : nums) {
+            if (pq.size() < k) {
+                pq.push(num);
+            } else {
+                if (pq.top() < num) {
+                    pq.pop();
+                    pq.push(num);
+                }
+            }
+        }
+    }
+    
+    int add(int val) {
+        if (pq.size() < this->k_) {
+            pq.push(val);
+        }
+        else if (pq.top() < val) {
+            pq.pop();
+            pq.push(val);
+        }
+
+        return pq.top();
+    }
+};
+
+/**
+ * Your KthLargest object will be instantiated and called as such:
+ * KthLargest* obj = new KthLargest(k, nums);
+ * int param_1 = obj->add(val);
+ */
