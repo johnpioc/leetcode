62c352a82426b07cca5646dd6100915b271dc513
John Pioc <johnpioc15@gmail.com>
2026-02-24 14:12:27 +1000

    96: unique binary search trees

---
+++ src/UniqueBinarySearchTress96.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int numTrees(int n) {
+        vector<int> count(n + 1, 0);
+        count[0] = 1;
+        count[1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                count[i] += count[j - 1] * count[i - j];
+            }
+        }
+
+        return count[n];
+    }
+};
